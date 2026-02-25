01d559e792c7d06b7f8d799cb7e388f801edc91e
Rafael Amaral <rafaglhf@gmail.com>
2026-02-25 17:57:29 -0300

    variaveis adicionadas

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,21 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String meuNome = "Rafael";
+        int minhaIdade = 26;
+        double minhaAltura = 1.63;
+        boolean estouAprendendoJava = true;
+        short numeroCurto = 100;
+        long numeroLongo = 10000000000L;
+        float numeroDecimalCurto = 3.14f;
+        char letra = 'R';
+
+        System.out.println("Meu nome é: " + meuNome);
+        System.out.println("Minha idade é: " + minhaIdade);
+        System.out.println("Minha altura é: " + minhaAltura);
+        System.out.println("Estou aprendendo Java: " + estouAprendendoJava);
+        System.out.println("Número curto: " + numeroCurto);
+        System.out.println("Número longo: " + numeroLongo);
+        System.out.println("Número decimal curto: " + numeroDecimalCurto);
+        System.out.println("Letra: " + letra);
     }
 }
